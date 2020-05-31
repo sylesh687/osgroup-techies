55171a229b4f31f8302227ee2c1436b4d0130cc8
Shailesh Thakur <timime687@gmail.com>
2020-05-31 15:20:53 +0545

    [Fix] [Summary] #Added fix for asg tfs

--- modules/aws/auto_scaling_groups/main.tf
+++ modules/aws/auto_scaling_groups/main.tf
@@ -6,3 +6,68 @@ Description: This tf Provisions Auto Scaling Group
 provider "aws"{
   region="${var.region}"
 }
+
+
+// ------------ AutoScaling Group----
+
+resource "aws_autoscaling_group" "auto_scaling_group" {
+  
+  name="${var.name}"
+  max_size="${var.max_size}"
+  min_size="${var.min_size}"
+  desired_capacity="${var.desired_capacity}"
+  vpc_zone_identifier=["${var.vpc_zone_identifier}"]
+  protect_from_scale_in="${var.protect_from_scale_in}"
+  termination_policies=["${var.termination_policies}"]
+
+  launch_template {
+
+        id="${var.launch_template_id}"
+        version="$Latest"
+  }
+ 
+  initial_lifecycle_hook{
+
+        name="Launch"
+        default_result="CONTINUE"
+        heartbeat_timeout="30"
+        lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+  }
+  
+  initial_lifecycle_hook{
+
+        name="Terminate"
+        default_result="CONTINUE"
+        heartbeat_timeout="30"
+        lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+
+   }
+
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "${var.environment}"
+      propagate_at_launch = true
+    },
+    {
+    key                 = "Application"
+      value               = "${var.application}"
+      propagate_at_launch = true
+    },
+
+    {
+    key                 = "Maintainer"
+      value               = "${var.maintainer}"
+      propagate_at_launch = true
+    },
+    {
+    key                 = "Component"
+      value               = "${var.component}"
+      propagate_at_launch = true
+    }
+
+  ]
+}
+
+  
--- modules/aws/auto_scaling_groups/variables.tf
+++ modules/aws/auto_scaling_groups/variables.tf
@@ -1,7 +1,38 @@
 /* 
-Varibles Declarations Are  Done Here
+Varibles Definitions
 */
 
-// Provider Related Variables
+//--------- Provider Related Variables ----
 
 variable "region" {}
+
+
+//--------- ASG Related Variables
+
+variable "name" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
+		
+// -- Points to "vpc_zone_identifier" --> refers to the VPC ID for the Group 
+
+variable "vpc_zone_identifier" {
+  default=[""]
+}
+
+variable "protect_from_scale_in" {}
+variable "termination_policies" {
+  default=[""]
+}
+variable "launch_template_id" {}
+
+
+// -------- Tag Related Variables ------
+
+variable "application" {}
+variable "environment" {}
+variable "component" {}
+variable "maintainer" {}
+
+
+
