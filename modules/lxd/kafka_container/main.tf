a62bf6fa95f0cd5ee982186e22d0f42205d441bb
Shailesh Thakur <timime687@gmail.com>
2020-06-06 10:59:19 +0545

    [ Feat ] [ Kafka/Logstash ] [ Summary ] # Added Relative Path

---
+++ modules/lxd/kafka_container/main.tf
@@ -0,0 +1,33 @@
+/*
+
+Descripton: This tf Creates linux container
+
+*/
+
+provider "lxd" {
+}
+
+resource "lxd_container" "con" {
+  name      = "${var.con_name}"
+  image     = "${var.img_name}"
+  ephemeral = false
+  config = {
+    "boot.autostart" = true
+  }
+  
+  limits={
+    cpu="${var.cpu}"
+    memory="${var.mem}"
+  
+  }
+  profiles = ["default"]
+
+  
+ provisioner "local-exec" {
+    
+    command = " echo \"[target] \n${lxd_container.con.ip_address}\" >hosts ; ansible-playbook  -i hosts ../playbooks/kafka.yml"
+ }
+
+}
+
+
