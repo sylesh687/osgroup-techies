47b600ad1c7f795c98d4fab13ec13d88cae70439
Shailesh Thakur <timime687@gmail.com>
2020-06-10 12:41:40 +0545

    [ Feat ] [ Jenkins ] [ Summary ] # Added Tf for Nginx and Handler in Role:

--- modules/docker/jenkins/main.tf
+++ modules/docker/jenkins/main.tf
@@ -5,11 +5,38 @@ Descripton: Docker Container
 */
 
 provider "docker" {
+
 }
 
-resource "docker_container" "this" {
 
-  image  =
+resource "docker_image" "image" {
+
+   name           ="${var.image_name}"
+   keep_locally   = true
+
+}
+
+resource "docker_container" "container" {
+
+  image  = "${docker_image.image.latest}"
+  name   = "${var.container_name"
+  restart = "on-failure"
+  memory  = "${var.memory}"
+
+
+  volumes {
+
+      host_path        = "${var.mountpath}"
+      container_path   = /var/jenkins_home
+      
+  
+  }
+
+  ports {
+    internal = 8080
+    external = "${var.port}"
+  }
+
 
 }
 
---
+++ modules/docker/jenkins/variables.tf
@@ -0,0 +1,15 @@
+
+variable "image_name" {
+}
+
+variable "container_name" {
+}
+
+variable "port" {
+}
+
+variable "mountpath" {
+}
+
+variable "memory" {
+}
