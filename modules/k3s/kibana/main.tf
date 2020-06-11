4919a317553d0d8a76f3a622664a7d4168777c03
Shailesh Thakur <timime687@gmail.com>
2020-06-11 23:16:13 +0545

    [ Feat ] [ Kubernetes ] [ Kibana ] Manifests Added

--- modules/k3s/kibana/main.tf
+++ modules/k3s/kibana/main.tf
@@ -3,6 +3,33 @@ provider "kubernetes" {
 }
 
 
+resource "kubernetes_service" "service" {
+
+  metadata {
+
+    name = "${var.name}"
+
+  }
+
+  spec {
+    
+   selector={
+     app = "${var.name}"
+   }
+
+  }
+
+  port {
+      port        = 8080
+      target_port = 5601
+    }
+
+
+  type = "NodePort"
+
+}
+
+
 resource "kubernetes_config_map" "config_map" {
 
   metadata {
