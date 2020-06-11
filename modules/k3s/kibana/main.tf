da062e1c4d2ca1d360306205674ae1cc2b338526
Shailesh Thakur <timime687@gmail.com>
2020-06-11 22:49:39 +0545

    [ Feat ] [ Kubernetes ] [ Kibana ] Manifests Added

---
+++ modules/k3s/kibana/main.tf
@@ -0,0 +1,19 @@
+provider "kubernetes" {
+}
+
+
+resource "kubernetes_config_map" "config_map" {
+
+  metadata {
+    name = "${var.name}"
+
+   }
+
+  data = {
+
+  "filebeat.yml" = ""${file("${path.module}/filebeat.yml")}""
+
+  }
+
+
+}
